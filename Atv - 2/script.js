6ba65ac8f0090676d947f06ffa449863fe97bdab
lucas-moratori <lucasmoratoriformal@gmail.com>
2026-09-25 16:05:06 -0300

    Delete Atv - 2 directory

--- Atv - 2/script.js
+++
@@ -1,7 +0,0 @@
-function mudarPreço() {
-    let qnt = document.getElementById("qntd").value;
-    let preço = 29.00;
-    let final = (qnt * preço);
-    document.getElementById("total").innerText = "Total: " + "R$ " + final.toFixed(2).replace('.', ',');
-}
-mudarPreço();
