0bd362e13fd452d82085a768d50dbbdd4b8e12ed
lucas-moratori <lucasmoratoriformal@gmail.com>
2026-09-25 16:03:58 -0300

    Delete Atvs - HTML/Atv -1 directory

--- Atvs - HTML/Atv -1/script.js
+++
@@ -1,9 +0,0 @@
-function testar() {
-    let n = document.getElementById("nome").value
-    document.getElementById("resultado").textContent = n;
-}
-
-function limpar() {
-    document.getElementById("resultado").textContent = "";
-    document.getElementById("nome").value = "";
-}
